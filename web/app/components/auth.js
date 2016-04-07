0e521322c7ec953fe18b20f1ab6c8ad5ec539746
LMO <lam@cphbusiness.dk>
2016-04-07 18:26:57 +0200

    small fixes

--- web/app/components/auth.js
+++ web/app/components/auth.js
@@ -7,7 +7,7 @@ angular.module('myApp.security', [])
 
           $scope.$on("NotAuthenticatedEvent", function (event, res) {
             $scope.$emit("logOutEvent");
-            console.log(res.data.error.message);
+            
             if (typeof res.data.error !== "undefined" && res.data.error.message) {
               if (res.data.error.message.indexOf("No authorization header") === 0) {
                 //Provide a friendly message
